1ae16a3c3c6f1abb9c8c377258b5f11319a1aef1
Ezequiel Ferreira Ramos Siqueira <zequi_08@hotmail.com>
2025-09-09 08:53:48 -0300

    Update programacao_bd.sql
    estoque produto

--- programacao_bd.sql
+++ programacao_bd.sql
@@ -1,1 +1,12 @@
+CREATE PROCEDURE listar_clientes()
+BEGIN
+  SELECT * FROM Cliente;
+END;
+
+CREATE TRIGGER atualiza_estoque
+AFTER INSERT ON Pedido
+FOR EACH ROW
+BEGIN
+  UPDATE Produto SET estoque = estoque - 1 WHERE id = NEW.produto_id;
+END;
 
