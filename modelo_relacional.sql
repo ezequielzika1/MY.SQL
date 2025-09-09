b3841f7ef4d60a348c89efb72ff1f16bcf5d046f
Ezequiel Ferreira Ramos Siqueira <zequi_08@hotmail.com>
2025-09-09 08:50:41 -0300

    Update modelo_relacional.sql
    TABELA CLIENTE PEDIDO

--- modelo_relacional.sql
+++ modelo_relacional.sql
@@ -1,1 +1,12 @@
+CREATE TABLE Cliente (
+  id INT PRIMARY KEY,
+  nome VARCHAR(100),
+  email VARCHAR(100)
+);
+
+CREATE TABLE Pedido (
+  id INT PRIMARY KEY,
+  cliente_id INT,
+  FOREIGN KEY (cliente_id) REFERENCES Cliente(id)
+);
 
